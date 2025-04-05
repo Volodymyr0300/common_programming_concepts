841d88398a2abcbee8ebfb1858bc5ed5cc7d6117
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-05 15:18:42 -0500

    compound types

--- com_prog_concepts/src/main.rs
+++ com_prog_concepts/src/main.rs
@@ -1,55 +1,23 @@
 fn main() {
-    // let mut x = 5;
-    // println!("The value of x is: {}", x);
-    // x = 6; // This will cause a compile-time error
-    // println!("The value of x is: {}", x);
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The values of are: {} {} {}", x, y, z);
+    
+    let tup2: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = tup2.0;
+    let six_point_four = tup2.1;
+    let one = tup2.2;
+    println!("The values of are: {} {} {}", five_hundred, six_point_four, one);
 
-    // const MAX_POINTS: u32 = 100_000;
-    // println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+    let _a = [1, 2, 3, 4, 5];
+    let first = _a[0];
+    let second = _a[1];
+    println!("The values of are: {} {}", first, second);
+    
+    let _months = ["January", "February", "March", "April", "May", "June", "July",
+"August", "September", "October", "November", "December"];
 
-    // let y = 5;
-    // let y = y + 1; // Shadowing
-    // let y = y * 2; // Shadowing
-    // println!("The value of y is: {}", y);
+    let _b: [i32; 5] = [1, 2, 3, 4, 5];
 
-    // let space = " ";
-    // let space = space.len();
-    // println!("The value of space is: {}", space);
-
-    // let mut spaces = "   ";
-    // spaces = spaces.len();
-    // println!("The value of spaces is: {}", spaces);
-
-    // let guess: u32 = "42".parse().expect("Not a number!");
-    // println!("The value of guess is: {}", guess);
-
-    let _x = 2.0; // f64
-    let _y: f32 = 3.0; // f32
-
-    // addition
-    let sum = 5 + 10;
-    // subtraction
-    let difference = 95.5 - 4.3;
-    // multiplication
-    let product = 4 * 30;
-    // division
-    let quotient = 56.7 / 32.2;
-    // remainder
-    let remainder = 43 % 5;
-
-    println!("Sum: {}", sum);
-    println!("Difference: {}", difference);
-    println!("Product: {}", product);
-    println!("Quotient: {}", quotient);
-    println!("Remainder: {}", remainder);
-
-    let t = true;
-    let f: bool = false; // with explicit type annotation
-
-    let c = 'z';
-    let z: char = 'Z'; // with explicit type annotation
-    let heart_eyed_cat = '😻';
-
-    println!("Boolean values: {} and {}", t, f);
-    println!("Characters: {} and {} and {}", c, z, heart_eyed_cat);
+    let _c = [3; 5]; // [3, 3, 3, 3, 3]
 }
