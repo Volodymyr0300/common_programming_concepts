702d474eecde8bebd54b4c8ab8ebd1b935ec1902
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-10 16:56:55 -0500

    loops

--- com_prog_concepts/src/main.rs
+++ com_prog_concepts/src/main.rs
@@ -1,57 +1,46 @@
-fn main() {
-    let number = 7;
-    if number < 5 {
-    println!("condition was true");
-    } else {
-    println!("condition was false");
-    }
-    }
-
 // fn main() {
-//     let number = 3;
-//     if number {
-//     println!("number was three");
+//     loop {
+//     println!("again!");
 //     }
 // }
 
 // fn main() {
-//     let number = 3;
-//     if number != 0 {
-//     println!("number was something other than zero");
-//     }
-// }
+//     let mut counter = 0;
 
-// fn main() {
-//     let number = 6;
-
-//     if number % 4 == 0 {
-//         println!("number is divisible by 4");
-//         } else if number % 3 == 0 {
-//         println!("number is divisible by 3");
-//         } else if number % 2 == 0 {
-//         println!("number is divisible by 2");
-//         } else {
-//         println!("number is not divisible by 4, 3, or 2");
-//     }
-// }
-
-// fn main() {
-//     let condition = true;
+//     let result = loop {
+//     counter += 1;
     
-//     let number = if condition {
-//     5
-//     } else {
-//     6
+//     if counter == 10 {
+//     break counter * 2;
+//     }
+    
 //     };
-//     println!("The value of number is: {}", number);
+
+//     println!("The result is {}", result);
 // }
 
 // fn main() {
-//     let condition = true;
-//     let number = if condition {
-//     5
-//     } else {
-//     "six"
-//     };
-//     println!("The value of number is: {}", number);
+//     let mut number = 3;
+//     while number != 0 {
+//     println!("{}!", number);
+//     number = number - 1;
+//     }
+//     println!("LIFTOFF!!!");
 // }
+
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+
+//     let mut index = 0;
+//     while index < 5 {
+//     println!("the value is: {}", a[index]);
+//     index = index + 1;
+//     }
+//     }
+
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+}
+}
